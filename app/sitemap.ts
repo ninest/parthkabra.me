f8233fba822f45ac3ab7e7e278f4e6b5c8a442c1
ninest <parth.kabra@gmail.com>
2025-06-18 15:24:44 -0400

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,41 @@
+import { getAllPosts, getAllProjects } from "@/modules/keystatic";
+import { MetadataRoute } from "next";
+
+const BASE_URL = "https://parthkabra.me";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projectPages: MetadataRoute.Sitemap = (await getAllProjects()).map((project) => ({
+    url: `${BASE_URL}/projects/${project.slug}`,
+    lastModified: project.updatedAt ?? project.createdAt,
+    priority: 0.8,
+    changeFrequency: "monthly",
+  }));
+  const blogPages: MetadataRoute.Sitemap = (await getAllPosts()).map((post) => ({
+    url: `${BASE_URL}/${post.slug}`,
+    lastModified: post.updatedAt ?? post.createdAt,
+    priority: 0.8,
+    changeFrequency: "monthly",
+  }));
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/me/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/me/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    ...projectPages,
+    ...blogPages,
+  ];
+}
