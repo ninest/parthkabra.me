7260fac5944a2dce1b6ec58f7a699a7d267fbf50
ninest <parth.kabra@gmail.com>
2026-04-28 18:45:10 -0400

    visible categories

---
+++ src/utils/visible-categories.ts
@@ -0,0 +1,30 @@
+import { getCollection } from "astro:content";
+
+export const visibleCategoryIds = [
+  "ai",
+  "astro",
+  "blog",
+  "cli",
+  "firefox",
+  "git",
+  "html",
+  "javascript",
+  "leetcode",
+  "mac",
+  "maps",
+  "meta",
+  "origami",
+  "python",
+  "typescript",
+  "ui",
+  "vscode",
+];
+
+export async function getVisibleCategories() {
+  const allCategories = await getCollection("categories");
+  const categoryById = new Map(allCategories.map((cat) => [cat.id, cat]));
+  return visibleCategoryIds.flatMap((id) => {
+    const category = categoryById.get(id);
+    return category ? [category] : [];
+  });
+}
